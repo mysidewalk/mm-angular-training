4f6b7e15c81d0881e5bfedfb383f372bbfa95bc7
dfowler <dfowler@mindmixer.com>
2015-01-27 15:20:36 -0600

    adding provisioner scripts

---
+++ etc/scripts/bootstrap.sh
@@ -0,0 +1,11 @@
+# Add apt source
+# -------------
+curl -sL https://deb.nodesource.com/setup | bash -
+
+# Install nodejs (includes npm)
+# -------------
+apt-get install -y nodejs
+
+# Install Bower
+# -------------
+npm install -g bower
