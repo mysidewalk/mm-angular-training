05dfcff20f3ce2e9b97a5d1e2cc0c4a81bbc7226
Rachel Hathaway <rhathaway@mindmixer.com>
2015-02-10 21:37:23 -0600

    02 add form

--- app/app.js
+++ app/app.js
@@ -1,12 +1,14 @@
 angular.module('app', ['ngRoute'])
 
-.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+.config(['$locationProvider', '$routeProvider', 
+  function($locationProvider, $routeProvider) {
 
-  $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
 
-  $routeProvider
-    .when('/', {
-      templateUrl: 'views/index.html'
-    });
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/index.html'
+      });
 
-}]);
+  }
+]);
