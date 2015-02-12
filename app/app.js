30ba0d14f7b51579e6b0b1e9c2c98570905928de
Rachel Hathaway <rhathaway@mindmixer.com>
2015-02-12 11:57:10 -0600

    update routes

--- app/app.js
+++ app/app.js
@@ -6,6 +6,9 @@ angular.module('app', ['ngRoute', 'controllers'])
     $routeProvider
       .when('/', {
         templateUrl: 'app/views/index.html'
+      })
+      .when('/clock', {
+        templateUrl: 'app/views/clock.html'
       });
 
   }
